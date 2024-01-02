e29a6d010fdf6ddba792922c015a35e084581d97
grantmerz <grantmerz97@gmail.com>
2024-01-02 11:58:08 -0600

    rename ROI heads

--- src/deepdisc/model/models.py
+++ src/deepdisc/model/models.py
@@ -200,9 +200,10 @@ class AugRedshiftPDFCasROIHeads(CascadeROIHeads):
             pred_instances = self.forward_with_given_boxes(features, pred_instances)
             pred_instances = self._forward_redshift(features, pred_instances)
             return pred_instances, {}
-
-
-class NewRedshiftPDFCasROIHeads(CascadeROIHeads):
+        
+        
+        
+class EBVRedshiftPDFCasROIHeads(CascadeROIHeads):
     """CascadeROIHead with added redshift pdf capability.  Follows the detectron2 CascadeROIHead class init, except for
 
     Parameters
@@ -285,6 +286,143 @@ class NewRedshiftPDFCasROIHeads(CascadeROIHeads):
         features = nn.Flatten()(features)
         ebvs = cat([x.gt_ebv for x in instances])
         features = torch.cat((features, ebvs.unsqueeze(1)), dim=-1)
+        
+        num_instances_per_img = [len(i) for i in instances]
+
+        if self.training:
+            fcs = self.redshift_fc(features)
+            pdfs = self.output_pdf(fcs)
+
+            gt_redshifts = cat([x.gt_redshift for x in instances])
+            nlls = -pdfs.log_prob(gt_redshifts) * self.zloss_factor
+
+            return {"redshift_loss": torch.mean(nlls)}
+
+        else:
+            # print(len(instances))
+            # print(len(instances[0]))
+            if len(instances[0]) == 0:
+                return instances
+            # for i, instances in enumerate(instances):
+            #    if num_instances_per_img[i] ==0:
+            #        continue
+            fcs = self.redshift_fc(features)
+            pdfs = self.output_pdf(fcs)
+            zs = torch.tensor(np.linspace(0, 5, 200)).to(fcs.device)
+
+            probs = torch.zeros((num_instances_per_img[0], 200)).to(fcs.device)
+            for i, z in enumerate(zs):
+                # probs.append(outputs.log_prob(z))
+                probs[:, i] = pdfs.log_prob(z)
+
+            for i, pred_instances in enumerate(instances):
+                pred_instances.pred_redshift_pdf = probs
+
+            return instances
+
+    def forward(self, images, features, proposals, targets=None):
+        del images
+        if self.training:
+            proposals = self.label_and_sample_proposals(proposals, targets)
+
+        if self.training:
+            # Need targets to box head
+            losses = self._forward_box(features, proposals, targets)
+            losses.update(self._forward_mask(features, proposals))
+            losses.update(self._forward_redshift(features, proposals))
+            losses.update(self._forward_keypoint(features, proposals))
+            return proposals, losses
+        else:
+            pred_instances = self._forward_box(features, proposals)
+            pred_instances = self.forward_with_given_boxes(features, pred_instances)
+            pred_instances = self._forward_redshift(features, pred_instances)
+            return pred_instances, {}
+
+
+
+class NewRedshiftPDFCasROIHeads(CascadeROIHeads):
+    """CascadeROIHead with added redshift pdf capability.  Follows the detectron2 CascadeROIHead class init, except for
+
+    Parameters
+    ----------
+    num_components : int
+        Number of gaussian components in the Mixture Density Network
+    """
+
+    # def __init__(self, cfg, input_shape):
+    def __init__(
+        self,
+        num_components: int,
+        zloss_factor: float,
+        *,
+        box_in_features: List[str],
+        box_pooler: ROIPooler,
+        box_heads: List[nn.Module],
+        box_predictors: List[nn.Module],
+        proposal_matchers: List[Matcher],
+        **kwargs,
+    ):
+        super().__init__(
+            box_in_features=box_in_features,
+            box_pooler=box_pooler,
+            box_heads=box_heads,
+            box_predictors=box_predictors,
+            proposal_matchers=proposal_matchers,
+            **kwargs,
+        )
+
+        self.redshift_pooler = ROIPooler(
+            output_size=7,
+            scales=tuple(k for k in [0.25, 0.125, 0.0625, 0.03125]),
+            sampling_ratio=0,
+            pooler_type="ROIAlignV2",
+        )
+
+        in_channels = 256
+        inshape = ShapeSpec(channels=in_channels, height=7, width=7)
+
+        self._output_size = (inshape.channels, inshape.height, inshape.width)
+        self.num_components = num_components
+        self.zloss_factor = zloss_factor
+
+
+
+        self.redshift_fc = nn.Sequential(
+            nn.Linear(np.prod(self._output_size), 1024),
+            nn.Tanh(),
+            nn.Linear(1024, 64),
+            nn.Tanh(),
+            nn.Linear(64, self.num_components * 3),
+            #nn.Softplus()
+        )
+
+    def output_pdf(self, inputs):
+        pdf = Independent(
+            MixtureSameFamily(
+                mixture_distribution=Categorical(logits=inputs[..., : self.num_components]),
+                component_distribution=Normal(
+                    inputs[..., self.num_components : 2 * self.num_components],
+                    #F.softplus(inputs[..., 2 * self.num_components :]),
+                    torch.exp(inputs[..., 2 * self.num_components :]),
+                ),
+            ),
+            0,
+        )
+        return pdf
+
+    def _forward_redshift(self, features, instances):
+        
+        if self.training:
+            instances, _ = select_foreground_proposals(instances, self.num_classes)
+        
+        if self.redshift_pooler is not None:
+            features = [features[f] for f in self.box_in_features]
+            boxes = [x.proposal_boxes if self.training else x.pred_boxes for x in instances]
+            features = self.redshift_pooler(features, boxes)
+
+        features = nn.Flatten()(features)
+        #ebvs = cat([x.gt_ebv for x in instances])
+        #features = torch.cat((features, ebvs.unsqueeze(1)), dim=-1)
         
         num_instances_per_img = [len(i) for i in instances]
 
