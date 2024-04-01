75e11113dace05ee4d1666ab613418f142852db4
grantmerz <grantmerz97@gmail.com>
2024-04-01 12:12:16 -0500

    update models

--- src/deepdisc/model/models.py
+++ src/deepdisc/model/models.py
@@ -548,6 +548,254 @@ class RedshiftPDFCasROIHeadsGold(CascadeROIHeads):
 
 
         
+        
+class RedshiftPDFCasROIHeadsGoldCRPS(CascadeROIHeads):
+    """CascadeROIHead with added redshift pdf capability.  Follows the detectron2 CascadeROIHead class init, except for
+
+    Parameters
+    ----------
+    num_components : int
+        Number of gaussian components in the Mixture Density Network
+    """
+
+    # def __init__(self, cfg, input_shape):
+    def __init__(
+        self,
+        num_components: int,
+        zloss_factor: float,
+        *,
+        box_in_features: List[str],
+        box_pooler: ROIPooler,
+        box_heads: List[nn.Module],
+        box_predictors: List[nn.Module],
+        proposal_matchers: List[Matcher],
+        **kwargs,
+    ):
+        super().__init__(
+            box_in_features=box_in_features,
+            box_pooler=box_pooler,
+            box_heads=box_heads,
+            box_predictors=box_predictors,
+            proposal_matchers=proposal_matchers,
+            **kwargs,
+        )
+
+        self.redshift_pooler = ROIPooler(
+            output_size=7,
+            scales=tuple(k for k in [0.25, 0.125, 0.0625, 0.03125]),
+            sampling_ratio=0,
+            pooler_type="ROIAlignV2",
+        )
+
+        in_channels = 256
+        inshape = ShapeSpec(channels=in_channels, height=7, width=7)
+
+        self._output_size = (inshape.channels, inshape.height, inshape.width)
+        self.num_components = num_components
+        self.zloss_factor = zloss_factor
+
+        
+        self.redshift_fc = nn.Sequential(
+            nn.Linear(np.prod(self._output_size), 1024),
+            nn.Tanh(),
+            nn.Linear(1024, 64),
+            nn.Tanh(),
+            nn.Linear(64, self.num_components * 3),
+            #nn.Softplus()
+        )
+        
+
+        '''
+        self.redshift_fc = nn.Sequential(
+            nn.Linear(np.prod(self._output_size), 64),
+            nn.Tanh(),
+            nn.Linear(64, 16),
+            nn.Tanh(),
+            nn.Linear(16, self.num_components * 3),
+            #nn.Softplus()
+        )
+        '''
+
+        
+        
+    def output_pdf(self, inputs):
+        pdf = Independent(
+            MixtureSameFamily(
+                mixture_distribution=Categorical(logits=inputs[..., : self.num_components]),
+                component_distribution=Normal(
+                    inputs[..., self.num_components : 2 * self.num_components],
+                    #F.softplus(inputs[..., 2 * self.num_components :]),
+                    torch.exp(inputs[..., 2 * self.num_components :]),
+                ),
+            ),
+            0,
+        )
+        return pdf
+    
+    def A(self, mu, std, true_zs):
+    
+        #n = norm(loc=mu, scale=std)
+        n = Independent(Normal(loc=mu, scale=std), 0)
+
+        A = (2*std*torch.exp(n.log_prob(true_zs)) 
+            + mu*(2*n.cdf(true_zs)-1))
+
+        #A = (2*std*torch.tensor(n.pdf(true_zs)) 
+        #    + mu*(2*n.cdf(true_zs)-1))
+
+        return A
+
+    '''
+    def CRPS_loss(self, inputs, true_zs):
+    
+        #loss
+
+        w = inputs[...,:self.num_components]
+        w = inputs[...,:self.num_components]
+        w = w - w.logsumexp(dim=-1, keepdim=True)
+        w = F.softmax(w, dim=-1)
+        mu = inputs[...,self.num_components:2*self.num_components]
+        std = torch.exp(inputs[...,2*self.num_components:])
+
+        losses = []
+        n = Normal(loc=0,scale=1)
+        
+        for q,x in enumerate(mu):
+            muq = mu[q]
+            stdq = std[q]
+            wq = w[q]
+
+            sx = (true_zs[q]-muq)/stdq
+            T1 = torch.sum(wq* ((2*stdq *torch.exp(n.log_prob(sx)))+ ((true_zs[q]-muq)*(2*n.cdf(sx)-1))))
+
+            wi = wq.unsqueeze(1)
+            wj = wq
+            mud = muq.unsqueeze(1)-muq
+            stdd = torch.sqrt(stdq.unsqueeze(1)**2 + stdq**2)
+            sx = (mud)/stdd
+            T2=torch.sum(wi*wj* ((2*stdd * torch.exp(n.log_prob(sx))) + (mud*(2*n.cdf(sx)-1))))
+
+            loss = torch.tensor([T1 - 0.5 * T2])
+            #loss = cat(loss)
+            losses.append(loss)
+        #print(cat(losses))
+        return cat(losses)
+    '''
+    
+    def CRPS_loss(self, inputs, true_zs):
+    
+        #This uses some tensor reshaping tricks for speedup (for loops are very slow)
+        #Based on the equations in D'Isanto et al 2018 Appendix A
+
+        w = inputs[...,:self.num_components]
+        w = w - w.logsumexp(dim=-1, keepdim=True)
+        w = F.softmax(w, dim=-1)
+        mu = inputs[...,self.num_components:2*self.num_components]
+        std = torch.exp(inputs[...,2*self.num_components:])
+
+        n = Normal(loc=0,scale=1)
+        sx = (true_zs.unsqueeze(1) - mu)/std
+        T1 = torch.sum(w* ((2*std *torch.exp(n.log_prob(sx)))+ ((true_zs.unsqueeze(1)-mu)*(2*n.cdf(sx)-1))), dim=-1)
+
+        wi = w.unsqueeze(-1)
+        wj = w.unsqueeze(1)
+        mud = mu.unsqueeze(-1)-mu.unsqueeze(1)
+        stdd = torch.sqrt(std.unsqueeze(1)**2 + std.unsqueeze(-1)**2)
+        n = Normal(loc=0,scale=1)
+        sx = (mud)/stdd
+        T2=torch.sum(wi*wj* ((2*stdd * torch.exp(n.log_prob(sx))) + (mud*(2*n.cdf(sx)-1))),dim=[-2,-1])
+        
+        losses = T1-0.5*T2
+        #print(losses)
+        return losses
+    
+    
+    def _forward_redshift(self, features, instances, targets=None):
+        
+        if self.training:
+            #Add all gt bounding boxes for redshift regression
+            proposals = add_ground_truth_to_proposals(targets, instances)
+            finstances, _ = select_foreground_proposals(proposals, self.num_classes)
+
+            #finstances, _ = select_foreground_proposals(instances, self.num_classes)
+
+            instances = []
+            for x in finstances:
+                gold_inst = x[x.gt_magi < 25.3]
+                instances.append(gold_inst)
+            if len(instances)==0:
+                return 0
+            
+        #print('instances ', len(instances[0]))
+        #sz = np.load('/home/g4merz/rail_deepdisc/sampled_zs_gold.npy')
+        #sampled_zs = instances[0].gt_redshift.detach().cpu().numpy()
+        #szs = np.concatenate([sz,sampled_zs])
+        #np.save('/home/g4merz/rail_deepdisc/sampled_zs_gold.npy', szs)
+                
+        if self.redshift_pooler is not None:
+            features = [features[f] for f in self.box_in_features]
+            boxes = [x.proposal_boxes if self.training else x.pred_boxes for x in instances]
+            features = self.redshift_pooler(features, boxes)
+        
+        features = nn.Flatten()(features)
+        
+        num_instances_per_img = [len(i) for i in instances]
+
+        if self.training:
+            fcs = self.redshift_fc(features)
+            #pdfs = self.output_pdf(fcs)
+
+            gt_redshifts = cat([x.gt_redshift for x in instances])
+            #print(gt_redshifts.size())
+            
+            #nlls = -pdfs.log_prob(gt_redshifts) * self.zloss_factor
+            #return {"redshift_loss": torch.mean(nlls)}
+            
+            losses = self.CRPS_loss(fcs,gt_redshifts)
+            return {"redshift_loss": torch.mean(losses)}
+
+        else:
+            # print(len(instances))
+            # print(len(instances[0]))
+            if len(instances[0]) == 0:
+                return instances
+                
+            fcs = self.redshift_fc(features)
+            pdfs = self.output_pdf(fcs)
+            zs = torch.tensor(np.linspace(0, 3, 300)).to(fcs.device)
+            nin = torch.as_tensor(np.array([num_instances_per_img]))
+            #probs = torch.zeros((num_instances_per_img[0], 200)).to(fcs.device)
+
+            inds = np.cumsum(num_instances_per_img)
+
+            probs = torch.zeros((torch.sum(nin), 300)).to(fcs.device)
+            for j, z in enumerate(zs):
+                probs[:, j] = pdfs.log_prob(z)
+
+            for i, pred_instances in enumerate(instances):
+                pred_instances.pred_redshift_pdf = np.split(probs,inds)[i]
+
+            return instances
+
+    def forward(self, images, features, proposals, targets=None):
+        del images
+        if self.training:
+            proposals = self.label_and_sample_proposals(proposals, targets)
+
+        if self.training:
+            # Need targets to box head
+            losses = self._forward_box(features, proposals, targets)
+            losses.update(self._forward_mask(features, proposals))
+            losses.update(self._forward_redshift(features, proposals, targets))
+            #losses.update(self._forward_redshift(features, proposals))
+            losses.update(self._forward_keypoint(features, proposals))
+            return proposals, losses
+        else:
+            pred_instances = self._forward_box(features, proposals)
+            pred_instances = self.forward_with_given_boxes(features, pred_instances)
+            pred_instances = self._forward_redshift(features, pred_instances)
+            return pred_instances, {}
+        
 class RedshiftPDFCasROIHeadsGoldUniform(CascadeROIHeads):
     """CascadeROIHead with added redshift pdf capability.  Follows the detectron2 CascadeROIHead class init, except for
 
